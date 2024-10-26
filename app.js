d6b8b3b0df74d51928ad0b523c4bad6f2116327d
nilank nikhil <nilanknikhil@gmail.com>
2024-10-26 23:55:11 +0530

    basic

---
+++ app.js
@@ -0,0 +1,11 @@
+var name = "nilank nikhil"
+var a = 10
+var b = 20
+// console.log(name)
+// console.log(a + b)
+
+// global is not a part of your v8 engine, global is outside and it is one of the superpower which is given to us by node.js 
+// console.log(global)
+// console.log(this) //empty object
+// console.log(globalThis) // same global object
+console.log(globalThis == global)
